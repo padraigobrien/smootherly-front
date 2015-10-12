a8ab3b333c2d29100e1a603d4ab048cf5deefbfd
padraigobrien <padraig@padraigobrien.com>
2015-10-12 16:03:23 +0100

    initial commit

---
+++ scripts/config.js
@@ -0,0 +1,35 @@
+/**
+ * HOMER - Responsive Admin Theme
+ * version 1.8
+ *
+ */
+
+function configState($stateProvider, $urlRouterProvider, $compileProvider) {
+
+    // Optimize load start with remove binding information inside the DOM element
+    $compileProvider.debugInfoEnabled(true);
+
+    // Set default state
+    $urlRouterProvider.otherwise("/login");
+    $stateProvider
+
+        // Dashboard - Main page
+        .state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "views/dashboard.html",
+            data: {
+                pageTitle: 'Dashboard'
+            }
+        })
+      .state('login', {
+            url: "/login",
+            templateUrl: "Login/login.html"
+      })
+}
+
+angular
+    .module('homer')
+    .config(configState)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
